bb46fc617ecaacf3c6ead9c9cfb714eb71abb5c1
Guilherme Hott <guilhermehott@gmail.com>
2014-10-18 18:06:44 -0300

    merge com o trabalho do Raphael

--- src/main/java/br/org/mj/sislegis/app/model/Comissao.java
+++ src/main/java/br/org/mj/sislegis/app/model/Comissao.java
@@ -2,11 +2,9 @@ package br.org.mj.sislegis.app.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Version;
 import javax.xml.bind.annotation.XmlRootElement;
 
 @Entity
@@ -20,12 +18,23 @@ public class Comissao implements AbstractEntity {
 	@Column(name = "id", updatable = false, nullable = false)
 	private Long id;
 
+	@Column(name = "sigla")
+	private String sigla;
+
 	public Long getId() {
 		return this.id;
 	}
 
 	public void setId(final Long id) {
 		this.id = id;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
 	}
 
 	@Override
