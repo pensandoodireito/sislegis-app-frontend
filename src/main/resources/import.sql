ca602b54fc38a0e083a4dd0e27c852fd138792a9
Aman Rathie <amanrathie@gmail.com>
2014-10-21 23:23:31 -0200

    Correcao de encoding

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,8 +1,8 @@
 insert into Reuniao (id, data, version) values (1, CURRENT_DATE, 0);
 
-insert into Proposicao (id, ano, autor, dataApresentacao, ementa, idProposicao, numero, tipo, origem) values (1, '2014', 'Parlamentar 1', CURRENT_DATE, 'Determina a concess√£o de aux√≠lio alimenta√ß√£o aos trabalhadores de empresas prestadoras de servi√ßos terceirizados, reguladas por Enunciado do Tribunal Superior do Trabalho.', 464139, '6607', 'PL', 'C');
-insert into Proposicao (id, ano, autor, dataApresentacao, ementa, idProposicao, numero, tipo, origem) values (2, '2014', 'Parlamentar 2', CURRENT_DATE, 'Disp√µe sobre o armazenamento de botij√µes de g√°s liquefeito de petr√≥leo-GLP e d√° outras provid√™ncias.', 20918, '4491', 'PL', 'C');
-insert into Proposicao (id, ano, autor, dataApresentacao, ementa, idProposicao, numero, tipo, origem) values (3, '2014', 'Parlamentar 3', CURRENT_DATE, 'Criminaliza condutas praticadas contra c√£es e gatos, e d√° outras provid√™ncias.', 529820, '2833', 'PL', 'C');
+insert into Proposicao (id, ano, autor, dataApresentacao, ementa, idProposicao, numero, tipo, origem) values (1, '2014', 'Parlamentar 1', CURRENT_DATE, 'Determina a concess„o de auxÌlio alimentaÁ„o aos trabalhadores de empresas prestadoras de serviÁos terceirizados, reguladas por Enunciado do Tribunal Superior do Trabalho.', 464139, '6607', 'PL', 'C');
+insert into Proposicao (id, ano, autor, dataApresentacao, ementa, idProposicao, numero, tipo, origem) values (2, '2014', 'Parlamentar 2', CURRENT_DATE, 'Dispıe sobre o armazenamento de botijıes de g·s liquefeito de petrÛleo-GLP e d· outras providÍncias.', 20918, '4491', 'PL', 'C');
+insert into Proposicao (id, ano, autor, dataApresentacao, ementa, idProposicao, numero, tipo, origem) values (3, '2014', 'Parlamentar 3', CURRENT_DATE, 'Criminaliza condutas praticadas contra c„es e gatos, e d· outras providÍncias.', 529820, '2833', 'PL', 'C');
 
 insert into Reuniao_Proposicao values (1, 1);
 insert into Reuniao_Proposicao values (1, 2);
