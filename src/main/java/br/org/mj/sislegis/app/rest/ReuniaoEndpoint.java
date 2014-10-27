468a40ee96f830a9d43eebeafd35981cdd902194
Raphael Santos <raphaelbsb1@gmail.com>
2014-10-27 11:07:22 -0200

    Merge com alterações do Aman

--- src/main/java/br/org/mj/sislegis/app/rest/ReuniaoEndpoint.java
+++ src/main/java/br/org/mj/sislegis/app/rest/ReuniaoEndpoint.java
@@ -1,17 +1,12 @@
 package br.org.mj.sislegis.app.rest;
 
-import java.io.Serializable;
 import java.text.ParseException;
 import java.util.Date;
 import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
-import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import javax.persistence.OptimisticLockException;
-import javax.persistence.PersistenceContext;
-import javax.persistence.TypedQuery;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -22,10 +17,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.Response.Status;
 import javax.ws.rs.core.UriBuilder;
 
-import br.org.mj.sislegis.app.model.Proposicao;
 import br.org.mj.sislegis.app.model.ProposicaoJSON;
 import br.org.mj.sislegis.app.model.Reuniao;
 import br.org.mj.sislegis.app.service.ProposicaoService;
@@ -35,11 +28,8 @@ import br.org.mj.sislegis.app.util.Conversores;
 /**
  * 
  */
-@Stateless
 @Path("/reuniaos")
 public class ReuniaoEndpoint {
-	@PersistenceContext(unitName = "sislegis-app-persistence-unit")
-	private EntityManager em;
 
 	@Inject
 	private Service<Reuniao> service;
@@ -50,7 +40,7 @@ public class ReuniaoEndpoint {
 	@POST
 	@Consumes("application/json")
 	public Response create(Reuniao entity) {
-		em.persist(entity);
+		service.save(entity);
 		return Response.created(
 				UriBuilder.fromResource(ReuniaoEndpoint.class)
 						.path(String.valueOf(entity.getId())).build()).build();
@@ -59,11 +49,7 @@ public class ReuniaoEndpoint {
 	@DELETE
 	@Path("/{id:[0-9][0-9]*}")
 	public Response deleteById(@PathParam("id") Long id) {
-		Reuniao entity = em.find(Reuniao.class, id);
-		if (entity == null) {
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		em.remove(entity);
+		service.deleteById(id);
 		return Response.noContent().build();
 	}
 
@@ -71,21 +57,7 @@ public class ReuniaoEndpoint {
 	@Path("/{id:[0-9][0-9]*}")
 	@Produces("application/json")
 	public Response findById(@PathParam("id") Long id) {
-		TypedQuery<Reuniao> findByIdQuery = em
-				.createQuery(
-						"SELECT DISTINCT r FROM Reuniao r LEFT JOIN FETCH r.listaProposicao WHERE r.id = :entityId ORDER BY r.id",
-						Reuniao.class);
-		findByIdQuery.setParameter("entityId", id);
-		Reuniao entity;
-		try {
-			entity = findByIdQuery.getSingleResult();
-		} catch (NoResultException nre) {
-			entity = null;
-		}
-		if (entity == null) {
-			return Response.status(Status.NOT_FOUND).build();
-		}
-		return Response.ok(entity).build();
+		return Response.ok(service.findById(id)).build();
 	}
 
 	@GET
@@ -98,7 +70,6 @@ public class ReuniaoEndpoint {
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		System.out.println(date);
 		List<ProposicaoJSON> lista = proposicaoService
 				.buscarProposicoesPorDataReuniao(date);
 
@@ -117,7 +88,7 @@ public class ReuniaoEndpoint {
 	@Consumes("application/json")
 	public Response update(Reuniao entity) {
 		try {
-			entity = em.merge(entity);
+			entity = service.save(entity);
 		} catch (OptimisticLockException e) {
 			return Response.status(Response.Status.CONFLICT)
 					.entity(e.getEntity()).build();
