5139431558249919e7df2da2be4385d1ac3ae853
Aman Rathie <amanrathie@gmail.com>
2014-11-19 21:12:30 -0200

    Retirando codigo desnecessario

--- src/main/webapp/scripts/controllers/equipeController.js
+++ src/main/webapp/scripts/controllers/equipeController.js
@@ -5,7 +5,6 @@ angular.module('sislegisapp').controller('EquipeController', function ($scope, $
     $scope.$location = $location;
     $scope.equipe = $scope.equipe || {};
     $scope.equipe.listaEquipeUsuario = $scope.equipe.listaEquipeUsuario || [];
-    $scope.usuarioEditado = false;
 
     $scope.usuariosList = UsuarioResource.queryAll(function(items){
         $scope.usuariosSelectionList = $.map(items, function(item) {
@@ -37,13 +36,11 @@ angular.module('sislegisapp').controller('EquipeController', function ($scope, $
     	}
     	
     	$scope.usuarioSelecionado = undefined;
-    	$scope.usuarioEditado = true;
     }
     
     $scope.removerUsuario = function(equipeUsuario) {
     	var index = $scope.equipe.listaEquipeUsuario.indexOf(equipeUsuario);
 		$scope.equipe.listaEquipeUsuario.splice(index, 1);
-		$scope.usuarioEditado = true;
     }
     
     $scope.get = function() {
