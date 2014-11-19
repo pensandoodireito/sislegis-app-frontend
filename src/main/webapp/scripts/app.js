4a8000033c3d8f5bf5c0fff3956a06f92dc9acbf
Guilherme Hott <guilhermehott@gmail.com>
2014-11-19 06:00:36 -0200

    limpando o projeto, retirando arquivos nao usados

--- src/main/webapp/scripts/app.js
+++ src/main/webapp/scripts/app.js
@@ -20,15 +20,6 @@ angular.module('sislegisapp',['ngRoute','ngResource','ngLocale','ui.bootstrap','
 	
     //TODO remover 
 	  .when('/buscarProposicao',{templateUrl:'views/Comissao/buscarProposicao.html',controller:'SearchComissaoController'})
-	  .when('/Comissaos/new',{templateUrl:'views/Comissao/detail.html',controller:'NewComissaoController'})
-	  .when('/Comissaos/edit/:ComissaoId',{templateUrl:'views/Comissao/detail.html',controller:'EditComissaoController'})
-	  .when('/Proposicaos',{templateUrl:'views/Proposicao/search.html',controller:'SearchProposicaoController'})
-	  .when('/Proposicaos/new',{templateUrl:'views/Proposicao/detail.html',controller:'NewProposicaoController'})
-	  .when('/Proposicaos/edit/:ProposicaoId',{templateUrl:'views/Proposicao/detail.html',controller:'EditProposicaoController'})
-	  .when('/Reuniaos',{templateUrl:'views/Reuniao/search.html',controller:'SearchReuniaoController'})
-	  .when('/Reuniaos/new',{templateUrl:'views/Reuniao/detail.html',controller:'NewReuniaoController'})
-	  .when('/Reuniaos/edit/:ReuniaoId',{templateUrl:'views/Reuniao/detail.html',controller:'EditReuniaoController'})
-	  
 	
 	  .otherwise({
 	    redirectTo: '/Reuniaos/gerenciar'
