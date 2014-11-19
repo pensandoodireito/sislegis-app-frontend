beb66f6a28e52e66e4af2299279574a8707400e4
Aman Rathie <amanrathie@gmail.com>
2014-11-19 20:38:46 -0200

    Elaboracao Normativa - Criar camada rest/serviço

---
+++ src/main/java/br/org/mj/sislegis/app/service/ElaboracaoNormativaService.java
@@ -0,0 +1,10 @@
+package br.org.mj.sislegis.app.service;
+
+import javax.ejb.Local;
+
+import br.org.mj.sislegis.app.model.ElaboracaoNormativa;
+
+@Local
+public interface ElaboracaoNormativaService extends Service<ElaboracaoNormativa> {
+
+}
