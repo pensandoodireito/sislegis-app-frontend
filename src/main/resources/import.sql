92cfb43d804dfad0096b6d3bd14078d438538298
Aman Rathie <amanrathie@gmail.com>
2014-11-23 13:02:38 -0200

    Gerando id a partir da sequence do hibernate ao inves de manual

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -7,36 +7,36 @@
 --insert into ReuniaoProposicao(dataReuniao, siglaComissao, idProposicao, idReuniao) values (CURRENT_DATE, 'C_TESTE', 1, 1);
 
 -- Posicionamentos
-insert into Posicionamento (nome, id) values ('Acompanhar Relator', 100);
-insert into Posicionamento (nome, id) values ('Acompanhar Substitutivo', 101);
-insert into Posicionamento (nome, id) values ('Contrário', 102);
-insert into Posicionamento (nome, id) values ('Contrário com Emendas', 103);
-insert into Posicionamento (nome, id) values ('Contrário', 104);
-insert into Posicionamento (nome, id) values ('Elaborar VTS', 105);
-insert into Posicionamento (nome, id) values ('Favorável', 106);
-insert into Posicionamento (nome, id) values ('Favorável com Emendas', 107);
-insert into Posicionamento (nome, id) values ('Indiferente', 108);
-insert into Posicionamento (nome, id) values ('Nada a opor', 109);
-insert into Posicionamento (nome, id) values ('Não é tema do MJ', 110);
-insert into Posicionamento (nome, id) values ('Previamente Contrário', 111);
-insert into Posicionamento (nome, id) values ('Previamente Favorável', 112);
-insert into Posicionamento (nome, id) values ('Previamente Indiferente', 113);
-insert into Posicionamento (nome, id) values ('Previamente Nada a opor', 114);
-insert into Posicionamento (nome, id) values ('Previamente Não é tema do MJ', 115);
-insert into Posicionamento (nome, id) values ('Seguir pela Rejeição', 116);
+insert into Posicionamento (nome, id) values ('Acompanhar Relator', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Acompanhar Substitutivo', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Contrário', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Contrário com Emendas', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Contrário', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Elaborar VTS', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Favorável', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Favorável com Emendas', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Indiferente', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Nada a opor', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Não é tema do MJ', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Previamente Contrário', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Previamente Favorável', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Previamente Indiferente', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Previamente Nada a opor', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Previamente Não é tema do MJ', nextval ('hibernate_sequence'));
+insert into Posicionamento (nome, id) values ('Seguir pela Rejeição', nextval ('hibernate_sequence'));
 
 -- Encaminhamentos
-insert into Encaminhamento (nome, id) values ('Agendar Reunião', 100);
-insert into Encaminhamento (nome, id) values ('Apenas Monitorar', 101);
-insert into Encaminhamento (nome, id) values ('Cobrar Posicionamento', 102);
-insert into Encaminhamento (nome, id) values ('Despachar Nota Técnica com o Secretário', 103);
-insert into Encaminhamento (nome, id) values ('Elaborar Nota Técnica', 104);
-insert into Encaminhamento (nome, id) values ('Elaborar VTS', 105);
-insert into Encaminhamento (nome, id) values ('Enviar E-mail', 106);
-insert into Encaminhamento (nome, id) values ('Fazer contato telefônico', 107);
-insert into Encaminhamento (nome, id) values ('Incluir na Pauta Prioritária', 108);
-insert into Encaminhamento (nome, id) values ('Pedir Retirada de Pauta', 109);
-insert into Encaminhamento (nome, id) values ('Retirar da Pauta do MJ', 110);
+insert into Encaminhamento (nome, id) values ('Agendar Reunião', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Apenas Monitorar', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Cobrar Posicionamento', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Despachar Nota Técnica com o Secretário', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Elaborar Nota Técnica', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Elaborar VTS', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Enviar E-mail', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Fazer contato telefônico', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Incluir na Pauta Prioritária', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Pedir Retirada de Pauta', nextval ('hibernate_sequence'));
+insert into Encaminhamento (nome, id) values ('Retirar da Pauta do MJ', nextval ('hibernate_sequence'));
 
 -- Tags
 insert into Tag (id) values('Ministério');
@@ -44,20 +44,14 @@ insert into Tag (id) values('Câmara');
 insert into Tag (id) values('Senado');
 
 -- Usuarios
-insert into Usuario(id, nome, email) values (100, 'João', 'joao@joao.com');
-insert into Usuario(id, nome, email) values (101, 'Maria', 'maria@maria.com');
-insert into Usuario(id, nome, email) values (102, 'André', 'andre@andre.com');
-insert into Usuario(id, nome, email) values (103, 'Marcos', 'marcos@marcos.com');
-insert into Usuario(id, nome, email) values (104, 'Felipe', 'felipe@felipe.com');
-insert into Usuario(id, nome, email) values (105, 'Paula', 'paula@paula.com');
+insert into Usuario(id, nome, email) values (nextval ('hibernate_sequence'), 'João', 'joao@joao.com');
+insert into Usuario(id, nome, email) values (nextval ('hibernate_sequence'), 'Maria', 'maria@maria.com');
+insert into Usuario(id, nome, email) values (nextval ('hibernate_sequence'), 'André', 'andre@andre.com');
+insert into Usuario(id, nome, email) values (nextval ('hibernate_sequence'), 'Marcos', 'marcos@marcos.com');
+insert into Usuario(id, nome, email) values (nextval ('hibernate_sequence'), 'Felipe', 'felipe@felipe.com');
+insert into Usuario(id, nome, email) values (nextval ('hibernate_sequence'), 'Paula', 'paula@paula.com');
 
 -- equipe
-insert into equipe(id, nome) values (100, 'Equipe 1');
-insert into equipe(id, nome) values (101, 'Equipe 2');
-insert into equipe(id, nome) values (102, 'Equipe 3');
-
--- equipe_usuario
-insert into equipe_usuario(equipe_id, usuario_id, isCoordenador) values (100, 100, true);
-insert into equipe_usuario(equipe_id, usuario_id, isCoordenador) values (100, 101, false);
-insert into equipe_usuario(equipe_id, usuario_id, isCoordenador) values (100, 102, false);
-insert into equipe_usuario(equipe_id, usuario_id, isCoordenador) values (100, 103, false);
+insert into equipe(id, nome) values (nextval ('hibernate_sequence'), 'Equipe 1');
+insert into equipe(id, nome) values (nextval ('hibernate_sequence'), 'Equipe 2');
+insert into equipe(id, nome) values (nextval ('hibernate_sequence'), 'Equipe 3');
