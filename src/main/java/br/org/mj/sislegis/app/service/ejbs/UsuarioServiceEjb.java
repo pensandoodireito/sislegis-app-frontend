0eeb10b36df62a25f08578b85d8b69906d8499ce
Guilherme Hott <guilhermehott@gmail.com>
2014-11-25 12:02:02 -0200

    configurando o autocomplete utilizando o angular-ui-bootstrap

--- src/main/java/br/org/mj/sislegis/app/service/ejbs/UsuarioServiceEjb.java
+++ src/main/java/br/org/mj/sislegis/app/service/ejbs/UsuarioServiceEjb.java
@@ -1,8 +1,11 @@
 package br.org.mj.sislegis.app.service.ejbs;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
 
 import br.org.mj.sislegis.app.model.Usuario;
 import br.org.mj.sislegis.app.service.AbstractPersistence;
@@ -21,6 +24,14 @@ public class UsuarioServiceEjb extends AbstractPersistence<Usuario, Long> implem
 	@Override
 	protected EntityManager getEntityManager() {
 		return em;
+	}
+
+	@Override
+	public List<Usuario> findByNome(String nome) {
+		TypedQuery<Usuario> findByIdQuery = em.createQuery("SELECT u FROM Usuario u WHERE upper(u.nome) like upper(:nome) ORDER BY u.nome ASC",
+				Usuario.class);
+		findByIdQuery.setParameter("nome", "%"+nome+"%");
+		return findByIdQuery.getResultList();
 	}
 
 }
