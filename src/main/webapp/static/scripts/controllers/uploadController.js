48c59a154200a36b88b17bf72e17cc74ab856e13
Aman Rathie <amanrathie@gmail.com>
2014-11-26 11:21:07 -0200

    Incrementando upload e criando arquivo de propriedades

--- src/main/webapp/static/scripts/controllers/uploadController.js
+++ src/main/webapp/static/scripts/controllers/uploadController.js
@@ -1,6 +1,13 @@
 angular.module('sislegisapp').controller('UploadController',
 		function($scope, $http, FileUploader) {
 
-			$scope.uploader = new FileUploader();
-
+			// Essa parte vai na controladora da 
+			$scope.uploader = new FileUploader( {
+			    url: '../rest/upload',
+			    autoUpload : 'true',
+			    onSuccessItem : function(item, response, status, headers) {
+			    	console.log(response);
+			    }
+			});
+			
 		});
