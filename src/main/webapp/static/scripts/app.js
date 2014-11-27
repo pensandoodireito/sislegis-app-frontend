c5169385a71d0ea6660d4dae647e739bd61f4eaf
Aman Rathie <amanrathie@gmail.com>
2014-11-27 20:41:25 -0200

    Elaboracao Normativa - Editar

--- src/main/webapp/static/scripts/app.js
+++ src/main/webapp/static/scripts/app.js
@@ -20,6 +20,7 @@ angular.module('sislegisapp',['ngRoute','ngResource','ngLocale','ui.bootstrap','
     .when('/Equipes/edit/:EquipeId',{templateUrl:'views/Equipe/detail.html',controller:'EquipeController'})
     .when('/ElaboracaoNormativa',{templateUrl:'views/ElaboracaoNormativa/search.html',controller:'SearchElaboracaoNormativaController'})
     .when('/ElaboracaoNormativa/new',{templateUrl:'views/ElaboracaoNormativa/detail.html',controller:'ElaboracaoNormativaController'})
+    .when('/ElaboracaoNormativa/edit/:ElaboracaoNormativaId',{templateUrl:'views/ElaboracaoNormativa/detail.html',controller:'ElaboracaoNormativaController'})
     .when('/Upload',{templateUrl:'views/Upload/upload.html',controller:'UploadController'})
     
 	
