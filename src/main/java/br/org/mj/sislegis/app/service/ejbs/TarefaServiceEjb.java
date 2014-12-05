82864277e79a89d369d3dda6b3dc2d1654b58a66
Aman Rathie <amanrathie@gmail.com>
2014-12-05 11:39:05 -0200

    Incrementando tela detalhes de tarefa

--- src/main/java/br/org/mj/sislegis/app/service/ejbs/TarefaServiceEjb.java
+++ src/main/java/br/org/mj/sislegis/app/service/ejbs/TarefaServiceEjb.java
@@ -27,6 +27,23 @@ public class TarefaServiceEjb extends AbstractPersistence<Tarefa, Long> implemen
 	protected EntityManager getEntityManager() {
 		return em;
 	}
+	
+	@Override
+	public Tarefa buscarPorId(Long idTarefa) {
+		TypedQuery<Tarefa> findByIdQuery = em.createQuery("SELECT t FROM Tarefa t WHERE t.id = :idTarefa", Tarefa.class);
+		findByIdQuery.setParameter("idTarefa", idTarefa);
+		List<Tarefa> resultList = findByIdQuery.getResultList();
+		
+		if (resultList.size() > 0) {
+			for (Tarefa tarefa : resultList) {
+				if (tarefa.getEncaminhamentoProposicao() != null) {
+					tarefa.getEncaminhamentoProposicao().getProposicao();
+				}
+			}
+			return resultList.get(0);
+		}
+		return null;
+	}
 
 	@Override
 	public List<Tarefa> buscarPorUsuario(Long idUsuario) {
