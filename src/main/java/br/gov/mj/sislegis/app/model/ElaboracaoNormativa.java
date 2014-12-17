61c42add58a74c2d18b2d4b6d968c3162a9b8106
Guilherme Hott <guilhermehott@gmail.com>
2014-12-16 22:15:02 -0200

    retirando cascade do origem

--- src/main/java/br/gov/mj/sislegis/app/model/ElaboracaoNormativa.java
+++ src/main/java/br/gov/mj/sislegis/app/model/ElaboracaoNormativa.java
@@ -91,7 +91,7 @@ public class ElaboracaoNormativa implements AbstractEntity  {
 	@ManyToOne(fetch = FetchType.EAGER)
 	private Usuario coAutor;
 
-	@ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+	@ManyToOne(fetch = FetchType.EAGER)
 	private OrigemElaboracaoNormativa origem;
 
 	@ManyToOne(fetch = FetchType.EAGER)
