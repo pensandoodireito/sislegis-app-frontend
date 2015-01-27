3a8228b16447973fd18fbcd0e224b16e4c246f77
Guilherme Hott <guilhermehott@gmail.com>
2015-01-26 23:46:08 -0200

    inclusao do filtro de tags na impressao da lista de proposicoes

--- src/main/webapp/static/scripts/controllers/gerenciarReuniaoController.js
+++ src/main/webapp/static/scripts/controllers/gerenciarReuniaoController.js
@@ -20,7 +20,7 @@ angular.module('sislegisapp').controller(
     
     $scope.allProposicoes = [];
     
-    $scope.tagsProposicaoSelecionadas = [];
+    $scope.filtroTags = [];
     $scope.tagsProposicao = TagResource.listarTodos();
     
 	$scope.infiniteScroll = {
@@ -293,6 +293,9 @@ angular.module('sislegisapp').controller(
         	},
         	filtroFavorita: function() {
         		return $scope.filtroFavorita;
+        	},
+        	filtroTags: function() {
+        		return $scope.filtroTags;
         	}
           }
         });
