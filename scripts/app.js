6cbc17bbb189d13739e29234288287e7b101567a
Paulo Jerônimo <paulojeronimo@gmail.com>
2015-06-30 19:49:18 +0000

    Ajustes para funcionamento do keycloak

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,58 @@
 'use strict';
 
-angular.module('sislegisapp',['ngRoute','ngResource','ngLocale','ui.bootstrap','ngTagsInput', 'angularFileUpload', 'ui.utils', 
-                              'toaster', 'angular.filter', 'checklist-model', 'angularjs-dropdown-multiselect','infinite-scroll', 'ui.mask'])
+var module = angular.module('sislegisapp',
+    [   'ngRoute',
+        'ngResource',
+        'ngLocale',
+        'ui.bootstrap',
+        'ngTagsInput',
+        'angularFileUpload',
+        'ui.utils',
+        'toaster',
+        'angular.filter',
+        'checklist-model',
+        'angularjs-dropdown-multiselect',
+        'infinite-scroll',
+        'ui.mask'
+    ]);
 
-  .config(['$routeProvider', function($routeProvider) {
+module.constant('BACKEND', 'http://localhost:8080/sislegis/rest');
+
+var auth = {};
+
+var logout = function(){
+    console.log('*** LOGOUT');
+    auth.loggedIn = false;
+    auth.authz = null;
+    window.location = auth.logoutUrl;
+};
+
+angular.element(document).ready(function ($http) {
+    console.log("*** here");
+    var keycloakAuth = new Keycloak('keycloak.json');
+    auth.loggedIn = false;
+
+    keycloakAuth.init({ onLoad: 'login-required' }).success(function () {
+        console.log('here login');
+        auth.loggedIn = true;
+        auth.authz = keycloakAuth;
+        auth.logoutUrl = keycloakAuth.authServerUrl + "/realms/" + keycloakAuth.realm + "/tokens/logout?redirect_uri=http://sislegis.local:8080";
+        module.factory('Auth', function() {
+            return auth;
+        });
+
+        angular.bootstrap(document, ["sislegisapp"]);
+
+    }).error(function () {
+            alert("failed to login");
+    });
+});
+
+module.controller('GlobalCtrl', function($scope, $http) {
+    $scope.logout = logout;
+});
+
+module.config(['$routeProvider', function($routeProvider) {
     $routeProvider
     	.when('/Reuniaos/reuniaonova',{templateUrl:'views/Reuniao/reuniaonova.html',controller:'GerenciarReuniaoController'})	
     	.when('/Reuniaos/gerenciar',{templateUrl:'views/Reuniao/gerenciar.html',controller:'GerenciarReuniaoController'})
@@ -53,15 +102,64 @@ angular.module('sislegisapp',['ngRoute','ngResource','ngLocale','ui.bootstrap','
 	    .otherwise({
 	    	redirectTo: '/Reuniaos/reuniaonova'
 	    });
-  }])
+}]);
   
-  .config(['$httpProvider', function($httpProvider) {
-	  $httpProvider.interceptors.push('HttpInterceptor');
-  }])
-  
-  .controller('NavController', function NavController($scope, $location) {
+module.controller('NavController', function NavController($scope, $location) {
 	  $scope.matchesRoute = function(route) {
 	      var path = $location.path();
 	      return (path === ("/" + route) || path.indexOf("/" + route + "/") == 0);
 	  };
-  });
+});
+
+module.factory('authInterceptor', function($q, Auth) {
+    return {
+        request: function (config) {
+            var deferred = $q.defer();
+            if (Auth.authz.token) {
+                Auth.authz.updateToken(5).success(function() {
+                    config.headers = config.headers || {};
+                    config.headers.Authorization = 'Bearer ' + Auth.authz.token;
+
+                    deferred.resolve(config);
+                }).error(function() {
+                        deferred.reject('Failed to refresh token');
+                    });
+            }
+            return deferred.promise;
+        }
+    };
+});
+
+module.factory('errorInterceptor', function($q) {
+    return function(promise) {
+        return promise.then(function(response) {
+            return response;
+        }, function(response) {
+            if (response.status == 401) {
+                console.log('session timeout?');
+                logout();
+            } else if (response.status == 403) {
+                alert("Forbidden");
+            } else if (response.status == 404) {
+                alert("Not found");
+            } else if (response.status) {
+                if (response.data && response.data.errorMessage) {
+                    alert(response.data.errorMessage);
+                } else {
+                    alert("An unexpected server error has occurred");
+                }
+            }
+            return $q.reject(response);
+        });
+    };
+});
+
+module.config(function($httpProvider) {
+    //$httpProvider.interceptors.push('HttpInterceptor');
+    //$httpProvider.defaults.headers.put['Access-Control-Allow-Origin'] = '*';
+
+    $httpProvider.responseInterceptors.push('errorInterceptor');
+    $httpProvider.interceptors.push('authInterceptor');
+
+});
+
