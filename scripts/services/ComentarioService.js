d1654a56d9f0a889c985ba403c68059cad9e4ef2
Rafael Coutinho <rafaelcoutinho@gmail.com>
2015-10-20 09:57:14 -0200

    Remove comentario json

--- scripts/services/ComentarioService.js
+++ scripts/services/ComentarioService.js
@@ -5,9 +5,16 @@ angular.module('sislegisapp').factory('ComentarioService', function($q, Comentar
 			comentario = new ComentarioResource(comentario);
 
 			comentario.dataCriacao = new Date();
-			comentario.idProposicao = idProposicao;
+			comentario.proposicao = {
+				id : idProposicao
+			};
 
 			ComentarioResource.save(comentario, function(data, responseHeaders) {
+				if (data.id == null) {
+					var loc = responseHeaders().location;
+					var idCreated = loc.substr(loc.lastIndexOf('/') + 1);
+					data.id = idCreated;
+				}
 				deferred.resolve(data, responseHeaders);
 			}, function(error) {
 				deferred.reject(error);
