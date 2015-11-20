e8f3da9179afe9ef21dfd8bf8744552618641c1f
Rafael Coutinho <rafaelcoutinho@gmail.com>
2015-11-20 13:10:18 -0200

    Fix no dropdown de notificacoes

--- scripts/controllers/notificacaoController.js
+++ scripts/controllers/notificacaoController.js
@@ -1,21 +1,39 @@
 angular.module('sislegisapp').controller('NotificacaoController',
 		function($scope, $routeParams, $location, $rootScope, $timeout, $modal, toaster, NotificacaoResource) {
+			$('.dropdown-toggle').click(function(e) {
+				e.preventDefault();
+				if ($scope.notificationsopen != true) {
+					$scope.notificationsopen = true;
+					$scope.onDropdown();
+				} else {
+					$scope.notificationsopen = false;
+					$scope.onDropup();
+				}
+				return false;
+			});
+			$scope.notificationsopen = false;
 			$scope.listaNotificacaoTarefas = [];
 			$(".notifications-menu").on("show.bs.dropdown", function(event) {
+				$scope.onDropdown();
+			});
+			$scope.onDropdown = function() {
 				for (var i = 0; i < $scope.listaNotificacaoTarefas.length; i++) {
 					var notificacao = $scope.listaNotificacaoTarefas[i];
+					console.log("checnadno ", notificacao, notificacao.visualizada);
 					if (!notificacao.visualizada) {
 						console.log("Agendando setador de visualizacao");
 						$scope.updateViewedHandler = $timeout($scope.setCurrentNotificationsViewed, 2000);
 						break;
 					}
 				}
-
-			});
-			$(".notifications-menu").on("hidden.bs.dropdown", function(event) {
+			};
+			$scope.onDropup = function() {
 				if ($scope.updateViewedHandler) {
 					$timeout.cancel($scope.updateViewedHandler);
 				}
+			};
+			$(".notifications-menu").on("hidden.bs.dropdown", function(event) {
+				$scope.onDropup();
 			});
 
 			$scope.setCurrentNotificationsViewed = function() {
